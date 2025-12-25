c75a4949f4f442562e3a1dcbb9a2d835bdd146a6
Ciprian Munteanu <ciprian@fastmail.com>
2025-12-25 10:05:59 +0200

    Add immutables

--- src/main/java/dev/ciprian/Main.java
+++ src/main/java/dev/ciprian/Main.java
@@ -1,5 +1,13 @@
 package dev.ciprian;
 
+import dev.ciprian.immutable.Foobar;
+import dev.ciprian.immutable.ImmutableFoobar;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Foobar foobar =
+                ImmutableFoobar.builder().foo(2).bar("Bar").addBuz(1, 3, 4).build();
+
+        System.out.println("foobar.foo() = " + foobar.foo());
+    }
 }
---
+++ src/main/java/dev/ciprian/immutable/Foobar.java
@@ -0,0 +1,16 @@
+package dev.ciprian.immutable;
+
+import java.util.List;
+import java.util.Set;
+import org.immutables.value.Value;
+
+@Value.Immutable
+public abstract class Foobar {
+    public abstract int foo();
+
+    public abstract String bar();
+
+    public abstract List<Integer> buz();
+
+    public abstract Set<Long> crux();
+}
